4635dc99e226033fd290f08cf8732a4db4d18035
Yiming Zhang <i@zhangyiming.tech>
2021-01-17 23:08:13 +0800

    添加键盘模拟函数

--- autoinput/ContentView.swift
+++ autoinput/ContentView.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftUI
+import Foundation
+
+func paste(waitTime:useconds_t=100000){
+    usleep(waitTime)
+    let push = CGEvent(keyboardEventSource: nil, virtualKey: 0x09, keyDown: true)
+    push?.flags = CGEventFlags.maskCommand
+    if let push = push {
+        push.post(tap: .cghidEventTap)
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            let str=NSPasteboard.general.string(forType: .string)
+            print("pasteboard:\(str ?? "null")")
+            sleep(3)
+            if str != nil{
+                for i in str!.indices{
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString("\(str![i])", forType: .string)
+                    paste()
+                }
+            }
+        }, label: {
+            Text("paste")
+        })
     }
 }
 
